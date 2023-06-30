02243a0db425a809886037f9608d5097a4efb1cc
Loloneuf <43267724+Loloneuf@users.noreply.github.com>
2023-06-30 11:33:44 +0200

    Update app.py

--- src/overcooked_demo/server/app.py
+++ src/overcooked_demo/server/app.py
@@ -647,7 +647,9 @@ def play_game(game: OvercookedGame, fps=6):
             )
             socketio.sleep(game.reset_timeout / 1000)
         else:
-            app.logger.info(game.mdp.explain.log_gradient())
+            socketio.emit(
+                "explanations", {"stuck": game.mdp.explain.collide, "infos":mdp.explain.log_gradient()}
+            )
             socketio.emit(
             "state_pong", {"state": game.get_state()}, room=game.id
             )
