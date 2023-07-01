6cfb868c1e025011c12a28fd1f09ce0fb9c5c9c0
Loloneuf <43267724+Loloneuf@users.noreply.github.com>
2023-07-01 18:45:21 +0200

    Update overcooked_mdp.py

--- src/overcooked_ai_py/mdp/overcooked_mdp.py
+++ src/overcooked_ai_py/mdp/overcooked_mdp.py
@@ -1109,7 +1109,7 @@ class OvercookedGridworld(object):
 
             else :
                 while objectif != "interact" or c<6 :
-                    next_state,info =self.overcooked_world.get_state_transition(self, prev_state, self.overcooked_world.action_to_overcooked_action[objectif])
+                    next_state,info =self.overcooked_world.get_state_transition(self, prev_state, self.action_to_overcooked_action[objectif])
                     objectif=model.action(next_state)
                     prev_state=next_state
                     c+=1
