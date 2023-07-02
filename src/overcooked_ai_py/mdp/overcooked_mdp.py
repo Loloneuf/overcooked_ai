eb400d3fc7bdb72626e53cc0c61f0ac608c7c4cc
Loloneuf <43267724+Loloneuf@users.noreply.github.com>
2023-07-03 01:20:23 +0200

    Update overcooked_mdp.py

--- src/overcooked_ai_py/mdp/overcooked_mdp.py
+++ src/overcooked_ai_py/mdp/overcooked_mdp.py
@@ -1110,7 +1110,7 @@ class OvercookedGridworld(object):
             objectif = npc_action
             list_action = [objectif,Action.STAY]
             next_state,info =self.overcooked_world.get_state_transition(prev_state, list_action)
-            #fin = model.action(next_state)
+            fin = model.action(next_state)
             '''if objectif == (0,1) :
                 objectif =  Direction.SOUTH
             elif objectif == (0,-1) : 
@@ -1147,7 +1147,7 @@ class OvercookedGridworld(object):
                     elif (key == "X"):
                         future = "I want to interact with the counter"'''
        
-            self.future= next_state #future + compl
+            self.future= fin #future + compl
 
         """
         def get_gradient(self,state,npc_action):
