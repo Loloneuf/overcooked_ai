f8896b06f6f1fec1513890660d8ed678d78dedae
Loloneuf <43267724+Loloneuf@users.noreply.github.com>
2023-07-09 19:41:41 +0200

    Update overcooked_mdp.py

--- src/overcooked_ai_py/mdp/overcooked_mdp.py
+++ src/overcooked_ai_py/mdp/overcooked_mdp.py
@@ -1109,7 +1109,7 @@ class OvercookedGridworld(object):
 
 
         def contrastive(self,model, state,npc_action):
-            dict={"soup": 0, "1ing":0, "2ing":0,"3ing":0,"ct":0,"Objp1":0,"p1onion":0,"p1soup":0,"p1dish":0,"Objp2":0,"p2ogn":0,"p2soup":0,"p2dish":0}
+            dict={"soup": 0, "1ing":0, "2ing":0,"3ing":0,"ct":0,"Objp1":0,"p1onion":0,"p1soup":0,"p1dish":0,"Objp2":0,"p2onion":0,"p2soup":0,"p2dish":0}
             if "soup" in state.unowned_objects_by_type.items() :
                 dict["soup"] = 1
                 dict[str(len(state.unowned_objects_by_type["soup"][0].ingredients)) + "ing"]=1
