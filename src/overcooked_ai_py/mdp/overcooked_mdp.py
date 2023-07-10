1c02944dda06492a99bb5afad46842fba1e3477b
Loloneuf <43267724+Loloneuf@users.noreply.github.com>
2023-07-10 10:51:58 +0200

    Update overcooked_mdp.py

--- src/overcooked_ai_py/mdp/overcooked_mdp.py
+++ src/overcooked_ai_py/mdp/overcooked_mdp.py
@@ -1,3 +1,4 @@
+
 import copy
 import itertools
 import warnings
@@ -1109,13 +1110,13 @@ class OvercookedGridworld(object):
 
 
         def contrastive(self,model, state,npc_action):
-            dict={"soup": 0, "1ing":0, "2ing":0,"3ing":0,"ct":0,"Objp1":0,"p1onion":0,"p1soup":0,"p1dish":0,"Objp2":0,"p2onion":0,"p2soup":0,"p2dish":0}
+            dict={"soup": 0, "1ing":0, "2ing":0,"3ing":0,"ct_idle":0,"ct":0,"Objp1":0,"p1onion":0,"p1soup":0,"p1dish":0,"Objp2":0,"p2onion":0,"p2soup":0,"p2dish":0}
             if "soup" in state.unowned_objects_by_type :
                 dict["soup"] = 1
                 soupstate = state.unowned_objects_by_type["soup"][0]
                 dict[str(len(soupstate.ingredients)) + "ing"]=1
                 if soupstate.is_idle():
-                    dict"ct_idle" =
+                    dict["ct_idle"] = 1
                 else:
                     dict["ct"]= (20 - soupstate.cook_time_remaining())*(1/20) #check is_cooking, modif le time_remain 
             if state.players[0].held_object != None : 
