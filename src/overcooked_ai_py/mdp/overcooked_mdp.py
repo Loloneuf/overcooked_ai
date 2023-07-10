756560b9ff072281952c71fec98f40ed5f03d47c
Loloneuf <43267724+Loloneuf@users.noreply.github.com>
2023-07-10 17:43:50 +0200

    Update overcooked_mdp.py

--- src/overcooked_ai_py/mdp/overcooked_mdp.py
+++ src/overcooked_ai_py/mdp/overcooked_mdp.py
@@ -1117,7 +1117,9 @@ class OvercookedGridworld(object):
                 if (dict["1ing"] == 0 and dict["2ing"] == 0 and dict["3ing"] == 0):
                     num_ing = 0
                 else:
-                    num_ing = n for key,value in dict.items() n=int(key[0]) if(bool(value) and key[1:]=="ing") 
+                    for key,value in dict.items():
+                        if(bool(value) and key[1:]=="ing"):
+                            num_ing = int(key[0])
 
                 if bool(dist["ct_idle"]):
                     ct=-1
